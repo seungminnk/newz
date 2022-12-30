f792fa942d2322df84e1d3a321a99512afbb66c6
haesungJoo <haesung0728@gmail.com>
2022-12-30 13:31:21 +0900

    [init] route 제어 코드 생성

---
+++ lib/application/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes{
+  static const splash = '/splash';
+  static const application = '/application';
+
+  static const home = '/home';
+  static const search = '/search';
+  static const mypage = '/mypage';
+}
---
+++ lib/application/routes/app_routes_info.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:newz/application/application_view.dart';
+import 'package:newz/application/routes/app_routes.dart';
+import 'package:newz/feature/home/home_view.dart';
+import 'package:newz/feature/mypage/mypage_view.dart';
+import 'package:newz/feature/search/search_view.dart';
+
+class AppRoutesInfo{
+  static Map<String, WidgetBuilder> details = {
+    // AppRoutes.splash : (context) => const ApplicationView(),
+    AppRoutes.application : (context) => const ApplicationView(),
+
+    AppRoutes.home : (context) => const HomeView(),
+    AppRoutes.search : (context) => const SearchView(),
+    AppRoutes.mypage : (context) => const MyPageView(),
+  };
+}
