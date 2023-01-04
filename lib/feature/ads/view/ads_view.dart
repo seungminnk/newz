5602a263e492ecddc836128418d206b5038b14a5
haesungJoo <haesung0728@gmail.com>
2023-01-04 18:29:48 +0900

    [init] native ad 를 위한 flutter 에서 처리해줘야 하는 부분 구현
    - 하나의 위젯으로 구현

---
+++ lib/feature/ads/view/ads_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:newz/feature/ads/util/AdHelperUtil.dart';
+
+class AdsView extends StatefulWidget {
+  const AdsView({Key? key}) : super(key: key);
+
+  @override
+  State<AdsView> createState() => _AdsViewState();
+}
+
+class _AdsViewState extends State<AdsView> {
+
+  NativeAd? _ad;
+
+  @override
+  Widget build(BuildContext context) {
+    return _ad == null ?
+      const Center(
+        child: SizedBox(
+          width: 20,
+          height: 20,
+          child: CircularProgressIndicator(),
+        ),
+      )
+          :
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Container(
+          height: 360,
+          padding: const EdgeInsets.all(20.0),
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+              color: Color.fromARGB(255, 232, 234, 246)
+          ),
+          child: AdWidget(ad: _ad!),
+        ),
+      );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    NativeAd(
+      adUnitId: AdHelperUtil.nativeAdUnitId,
+      factoryId: "NewzComponentAd",
+      request: const AdRequest(),
+      listener: NativeAdListener(
+        onAdLoaded: (responseAd){
+          setState(() {
+            _ad = responseAd as NativeAd;
+          });
+        },
+        onAdFailedToLoad: (responseAd, loadAdError){
+          print("error code : ${loadAdError.code}, error message : ${loadAdError.message}");
+        }
+      ),
+    );
+  }
+}
+
