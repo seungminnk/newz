2d7e14a85456fc21b2dcafd1ae8259c11e7aea86
haesungJoo <haesung0728@gmail.com>
2023-01-04 18:29:12 +0900

    [init] native ad 를 위한 android 에서 처리해줘야 하는 부분 구현
    - layout 생성
    - response 에 따른 resource 맵핑
    - `factory-id` 플러터에서 호출하기 위한 아이디 선언

--- android/app/src/main/kotlin/com/newz/newz/MainActivity.kt
+++ android/app/src/main/kotlin/com/newz/newz/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.newz.newz
 
+import com.newz.newz.ad_factory.NewzComponentAd
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        GoogleMobileAdsPlugin.registerNativeAdFactory(
+            flutterEngine, "NewzComponentAd", NewzComponentAd(context))
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        super.cleanUpFlutterEngine(flutterEngine)
+
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "NewzComponentAd")
+    }
 }
