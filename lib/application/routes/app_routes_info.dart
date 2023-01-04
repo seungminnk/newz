b93fc8bea004e54b6d893a81855999567a9b2a84
haesungJoo <haesung0728@gmail.com>
2023-01-04 19:01:37 +0900

    [feature] application_view 파일 view 폴더 내로 이동

--- lib/application/routes/app_routes_info.dart
+++ lib/application/routes/app_routes_info.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:newz/feature/application/application_view.dart';
+import 'package:newz/feature/application/view/application_view.dart';
 import 'package:newz/application/routes/app_routes.dart';
 import 'package:newz/feature/home/view/home_view.dart';
 import 'package:newz/feature/mypage/view/mypage_view.dart';
