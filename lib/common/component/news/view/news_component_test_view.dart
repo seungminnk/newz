02b201d8bfed69df5439d5ed971219032369f11b
sggnology <sggnology@gmail.com>
2023-01-13 22:29:01 +0900

    [view] news 공통 컴포넌트 구성중

---
+++ lib/common/component/news/view/news_component_test_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:newz/common/component/news/view/news_summary_component_view.dart';
+
+class NewsComponentTestView extends StatelessWidget {
+  const NewsComponentTestView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('뉴스 컴포넌트 테스트')),
+      body: Center(
+        child: NewsSummaryComponentView()
+      )
+    );
+  }
+}
