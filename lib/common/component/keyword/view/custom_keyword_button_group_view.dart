de96f6e4bca6da2f1acc8b1894bda8a58d63dd8e
sggnology <sggnology@gmail.com>
2023-01-15 17:21:31 +0900

    [common > component] 키워드 라디오 버튼 그룹 view 구현

---
+++ lib/common/component/keyword/view/custom_keyword_button_group_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:newz/common/component/keyword/model/keyword_radio_model.dart';
+import 'package:newz/common/component/keyword/view/keyword_button_shape_view.dart';
+
+typedef _ClickedCb = void Function(KeywordRadioModel clickedValue);
+
+class CustomKeywordButtonGroupView extends StatefulWidget {
+  const CustomKeywordButtonGroupView({
+    required this.keywordRadioModelList,
+    required this.clickCb,
+    Key? key,
+  }) : super(key: key);
+
+  final List<KeywordRadioModel> keywordRadioModelList;
+  final _ClickedCb clickCb;
+
+  @override
+  State<CustomKeywordButtonGroupView> createState() => _CustomKeywordButtonGroupViewState();
+}
+
+class _CustomKeywordButtonGroupViewState extends State<CustomKeywordButtonGroupView> {
+  List<KeywordRadioModel> sampleData = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    for (var element in widget.keywordRadioModelList) {
+      sampleData.add(element);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Wrap(
+      children: _buildKeywordButtons(sampleData, widget.clickCb),
+    );
+  }
+
+  Widget _keywordButtonWrappedView(List<KeywordRadioModel> keywordRadioModelList, _ClickedCb clickedCb, index){
+    return InkWell(
+      borderRadius: BorderRadius.circular(8.0),
+      onTap: () {
+        setState(() {
+          for (var keywordRadioModel in keywordRadioModelList) {
+            keywordRadioModel.isSelected = false;
+          }
+          keywordRadioModelList[index].isSelected = true;
+          clickedCb(keywordRadioModelList[index]);
+        });
+
+      },
+      child: KeywordButtonShapeView(keywordRadioModelList[index]),
+    );
+  }
+
+  List<Widget> _buildKeywordButtons(List<KeywordRadioModel> keywordRadioModelList, _ClickedCb clickedCb){
+    List<Widget> widgetList = [];
+    int lastIndex = keywordRadioModelList.length - 1;
+
+    for(int index = 0; index < keywordRadioModelList.length; index++){
+      widgetList.add(
+          _keywordButtonWrappedView(keywordRadioModelList, clickedCb, index)
+      );
+
+      if(index != lastIndex){
+        widgetList.add(const SizedBox(width: 20));
+      }
+    }
+
+    return widgetList;
+  }
+}
