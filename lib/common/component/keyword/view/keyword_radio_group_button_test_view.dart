fd4ba7cf1916588de437afc87bb8dba40c16771d
sggnology <sggnology@gmail.com>
2023-01-15 17:21:47 +0900

    [feature > test] 키워드 그룹 테스트를 위한 뷰 구성

--- lib/common/component/keyword/view/keyword_radio_group_button_test_view.dart
+++ lib/common/component/keyword/view/keyword_radio_group_button_test_view.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:newz/common/component/keyword/view/keyword_custom_button_group_view.dart';
+import 'package:newz/common/component/keyword/model/keyword_radio_model.dart';
+import 'package:newz/common/component/keyword/view/custom_keyword_button_group_view.dart';
 
 class KeywordRadioGroupButtonTestView extends StatelessWidget {
   const KeywordRadioGroupButtonTestView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const KeywordCustomButtonGroupView();
+
+    final List<KeywordRadioModel> list = [];
+    list.add(KeywordRadioModel(false, 'AAAAA', 'April 18'));
+    list.add(KeywordRadioModel(false, 'BBBBB', 'April 17'));
+    list.add(KeywordRadioModel(false, 'CCCCC', 'April 16'));
+    list.add(KeywordRadioModel(false, 'DDDDD', 'April 15'));
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('키워드 그룹 테스트')),
+      body: CustomKeywordButtonGroupView(
+        keywordRadioModelList: list,
+        clickCb: (modelValue){
+          print("중요한 손님 지나갑니다! : ${modelValue.value}");
+        },
+      ),
+    );
   }
 }
