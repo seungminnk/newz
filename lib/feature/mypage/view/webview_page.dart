23d9106055c06e67983e5859dab984bf10123027
tngudzzz <63445173+tngudzzz@users.noreply.github.com>
2023-01-27 16:04:06 +0900

    뉴스기사 스크랩 시 팜업메시지 구현

--- lib/feature/mypage/view/webview_page.dart
+++ lib/feature/mypage/view/webview_page.dart
@@ -33,6 +33,7 @@ class _WebviewWidgetState extends State<WebviewWidget> {
         actions: [
           IconButton(
               onPressed: () {
+                scrapDialog(context);
                 mypageController.webViewScapBtn(widget.weburl);
               },
               icon: SvgPicture.asset("assets/icons/scrap.svg"))
@@ -46,5 +47,116 @@ class _WebviewWidgetState extends State<WebviewWidget> {
         ),
       ),
     );
+  }
+
+  Future<dynamic> scrapDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: ((BuildContext context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            content: SizedBox(
+              height: 260,
+              width: 312,
+              child: Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                        width: 160,
+                        height: 120,
+                        child: Image.asset('assets/images/IMAGE.png')),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      width: 264,
+                      height: 123,
+                      child: Column(
+                        children: [
+                          const Text(
+                            "저장 완료",
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontSize: 18,
+                              color: Color(0xff37474f),
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          const Text(
+                            "관심기사를 '스크랩'에 저장하였습니다.",
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontSize: 14,
+                              color: Color(0xff78909C),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  Get.back();
+                                },
+                                child: Container(
+                                  width: 128,
+                                  height: 44,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 24, vertical: 12),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: const Color(0xFF3F51B5),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      '확인',
+                                      style: TextStyle(
+                                        fontFamily: 'Pretendard',
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Get.back();
+                                },
+                                child: Container(
+                                  width: 128,
+                                  height: 44,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 24, vertical: 12),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: const Color(0xFFC5CAE9),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      '취소',
+                                      style: TextStyle(
+                                        fontFamily: 'Pretendard',
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }));
   }
 }
