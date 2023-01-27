5a02ffc0a1f809a0a188ee8713dc4a7a2c841f7e
sggnology <sggnology@gmail.com>
2023-01-27 18:22:08 +0900

    [feature] application view 함수형 위젯 삭제

--- lib/feature/application/view/application_view.dart
+++ lib/feature/application/view/application_view.dart
@@ -15,99 +15,64 @@ class ApplicationView extends GetView<ApplicationController> {
 
   @override
   Widget build(BuildContext context) {
-    // Get.put(ApplicationController());
 
     return Scaffold(
-      body: _buildPageView(),
-      bottomNavigationBar: _buildBottomNavigationBar(controller),
-    );
-
-    // return Obx(() => Scaffold(
-    //       body: UserDataController.to.didSelectKeywords.value
-    //           ? _buildPageView()
-    //           : const IntroductionView(),
-    //       bottomNavigationBar: UserDataController.to.didSelectKeywords.value
-    //           ? _buildBottomNavigationBar(controller)
-    //           : null,
-    //     ));
-  }
-
-  PageView _buildPageView() {
-    return PageView(
-      controller: controller.applicationPageController,
-      physics: const NeverScrollableScrollPhysics(),
-      onPageChanged: controller.pageChanged,
-      children: const [
-        HomeView(),
-        SearchView(),
-        MyPageView(),
-        // TestPageView(),
-      ],
-    );
-  }
-
-  // FIXME 함수형 위젯 사용 지양
-  // FIXME 네비게이션바 아이콘, 디자인 수정 필요
-  Widget _buildBottomNavigationBar(ApplicationController controller) {
-    return SizedBox(
-      height: 64,
-      child: Obx(
-        () => BottomNavigationBar(
-          backgroundColor: const Color.fromARGB(255, 236, 239, 241),
-          currentIndex: controller.bottomNavigationBarIndex.value,
-          onTap: controller.bottomNavigationBarOnTap,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: [
-            BottomNavigationBarItem(
-              label: "",
-              icon: SvgPicture.asset(
-                "assets/icons/home.svg",
-                color: notActiveColor,
+      body: PageView(
+        controller: controller.applicationPageController,
+        physics: const NeverScrollableScrollPhysics(),
+        onPageChanged: controller.pageChanged,
+        children: const [
+          HomeView(),
+          SearchView(),
+          MyPageView(),
+          // TestPageView(),
+        ],
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 64,
+        child: Obx(
+          () => BottomNavigationBar(
+            backgroundColor: const Color.fromARGB(255, 236, 239, 241),
+            currentIndex: controller.bottomNavigationBarIndex.value,
+            onTap: controller.bottomNavigationBarOnTap,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: [
+              BottomNavigationBarItem(
+                label: "",
+                icon: SvgPicture.asset(
+                  "assets/icons/home.svg",
+                  color: notActiveColor,
+                ),
+                activeIcon: SvgPicture.asset(
+                  "assets/icons/home.svg",
+                  color: activeColor,
+                ),
               ),
-              activeIcon: SvgPicture.asset(
-                "assets/icons/home.svg",
-                color: activeColor,
+              BottomNavigationBarItem(
+                label: "",
+                icon: SvgPicture.asset(
+                  "assets/icons/search.svg",
+                  color: notActiveColor,
+                ),
+                activeIcon: SvgPicture.asset(
+                  "assets/icons/search.svg",
+                  color: activeColor,
+                ),
               ),
-            ),
-            BottomNavigationBarItem(
-              label: "",
-              icon: SvgPicture.asset(
-                "assets/icons/search.svg",
-                color: notActiveColor,
+              BottomNavigationBarItem(
+                label: "",
+                icon: SvgPicture.asset(
+                  "assets/icons/user.svg",
+                  color: notActiveColor,
+                ),
+                activeIcon: SvgPicture.asset(
+                  "assets/icons/user.svg",
+                  color: activeColor,
+                ),
               ),
-              activeIcon: SvgPicture.asset(
-                "assets/icons/search.svg",
-                color: activeColor,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "",
-              icon: SvgPicture.asset(
-                "assets/icons/user.svg",
-                color: notActiveColor,
-              ),
-              activeIcon: SvgPicture.asset(
-                "assets/icons/user.svg",
-                color: activeColor,
-              ),
-            ),
-            // BottomNavigationBarItem(
-            //   label: "",
-            //   icon: SizedBox(
-            //     width: 23,
-            //     height: 23,
-            //     child: SvgPicture.asset("assets/icons/experiment.svg"),
-            //   ),
-            //   activeIcon: SizedBox(
-            //       width: 23,
-            //       height: 23,
-            //       child: SvgPicture.asset(
-            //         "assets/icons/experiment.svg",
-            //         color: Colors.lightBlue,
-            //       )),
-            // ),
-          ],
+            ],
+          ),
         ),
       ),
     );
